cd9864671de2c9d4fcc3e914a19915c6c4a90cde
Juana Villalpando <juana.v14@gmail.com>
2020-01-31 08:50:22 -0800

    hook up welcome page and character page with react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Router>
+        <Link to="/WelcomePage">Welcome Page</Link>
+        <Link to="/">CharacterList</Link>
+        <Route exact path="/WelcomePage" component={WelcomePage}/>
+        <Route exact path="/" component={CharacterList}/>
+      </Router>
     </main>
   );
 }
